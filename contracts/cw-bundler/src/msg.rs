8d2bfd97552776a84548514a0abaa42bcc263de4
Peter Kim <p@peter-kim.com>
2022-01-28 12:33:54 -0800

    working on contract skeleton

---
+++ contracts/cw-bundler/src/msg.rs
@@ -0,0 +1,33 @@
+use cw721_base::msg::MintMsg as Cw721MintMsg;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub name: String,
+    pub symbol: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Increment {},
+    Reset { count: i32 },
+    Mint {},
+    DepositCW20 {},
+    DepositCW721 {},
+    DepositCW1155 {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetCount {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CountResponse {
+    pub count: i32,
+}
---
+++ contracts/cw-bundler/src/state.rs
@@ -0,0 +1,15 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::Addr;
+use cw_storage_plus::Item;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub count: i32,
+    pub owner: Addr,
+    pub name: String,
+    pub symbol: String,
+}
+
+pub const STATE: Item<State> = Item::new("state");
